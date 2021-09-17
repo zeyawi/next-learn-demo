8292b3bc40d5a2c9d13476c1b48d32c22df3e2a8
韦泽杨 <2468266670@qq.com>
2021-09-17 11:07:10 +0800

    add profile

--- components/layout.js
+++ components/layout.js
@@ -27,7 +27,7 @@ export default function Layout({children, home}){
                     <>
                         <Image
                             priority
-                            src="/images/profile.jpg"
+                            src="/images/g3.jpeg"
                             className={utilStyles.borderCircle}
                             height={144}
                             width={144}
